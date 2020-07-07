24ac45ec2186c263703150f400c3dda19e98267c
tom <tom@sapletta.com>
2020-07-07 21:33:00 +0200

    first version

--- server.py
+++ server.py
@@ -26,6 +26,7 @@ auth = HTTPBasicAuth()
 
 
 ip = "127.0.0.1"
+ip = "93.90.201.35"
 # config = "..\\config\\app.json"
 config_server = os.path.join('..', 'config', 'server.json')
 config_app = os.path.join('..', 'config', 'app.json')
