fa278195c9ef57c0c5fd398aa12d8ce0a04b9ad9
tom <tom@sapletta.com>
2020-07-07 22:52:47 +0200

    first version

--- server.py
+++ server.py
@@ -26,7 +26,8 @@ auth = HTTPBasicAuth()
 
 
 # ip = "127.0.0.1"
-ip = "93.90.201.35"
+# ip = "93.90.201.35"
+ip = "api.faas.ovh"
 # config = "..\\config\\app.json"
 config_server = os.path.join('..', 'config', 'server.json')
 config_app = os.path.join('..', 'config', 'app.json')
