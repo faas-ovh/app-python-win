01325ac79b3f2080c8d74ae4b234528d87fc71bf
tom <tom@sapletta.com>
2020-07-22 19:01:36 +0200

    more options for environment

--- server.py
+++ server.py
@@ -68,9 +68,9 @@ class processClass:
         # This might take several minutes to complete
         # print(self.command)
         clientCommand(self.client, self.command, self.folder, self.env)
-        time.sleep(5)
+        # time.sleep(5)
         # print("stop")
-        clientCommand(self.client, "stop", self.folder, self.env)
+        # clientCommand(self.client, "stop", self.folder, self.env)
         # p = multiprocessing.Process(target=clientCommand, name="clientCommand", args=(client, key, folder, env))
         # p.start()
         # time.sleep(5)
