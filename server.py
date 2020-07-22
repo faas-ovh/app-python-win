8d2f4be0c401ae5bfedaf39b0d7e1dd2666829a0
tom <tom@sapletta.com>
2020-07-22 19:00:09 +0200

    more options for environment

--- server.py
+++ server.py
@@ -67,24 +67,24 @@ class processClass:
         #
         # This might take several minutes to complete
         # print(self.command)
-        # clientCommand(self.client, self.command, self.folder, self.env)
-        # time.sleep(10)
+        clientCommand(self.client, self.command, self.folder, self.env)
+        time.sleep(10)
         # print("stop")
-        # clientCommand(self.client, "stop", self.folder, self.env)
+        clientCommand(self.client, "stop", self.folder, self.env)
         # p = multiprocessing.Process(target=clientCommand, name="clientCommand", args=(client, key, folder, env))
         # p.start()
         # time.sleep(5)
         # p.terminate()
         # p.join()
         # Start foo as a process
-        p = multiprocessing.Process(target=clientCommand, name="clientCommand", args=(self.client, self.command, self.folder, self.env))
-        p.start()
+        # p = multiprocessing.Process(target=clientCommand, name="clientCommand", args=(self.client, self.command, self.folder, self.env))
+        # p.start()
         # Wait 10 seconds for foo
-        time.sleep(10)
+        # time.sleep(5)
         # Terminate foo
-        p.terminate()
+        # p.terminate()
         # Cleanup
-        p.join()
+        # p.join()
         self.client.close()
 
 
